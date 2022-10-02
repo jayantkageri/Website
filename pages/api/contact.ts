fad5c3fcccec758f52eabfe24bebbf0f6918d10e
Dhruv Arora <101944782+itsdhruvarora@users.noreply.github.com>
2022-10-02 17:23:37 +0530

    Optimizations and Updated the Dependencies

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -82,7 +82,6 @@ async function mail(id: number, name: string, email: string, ip: string | null, 
         const txt = `
 New Contact Request
 You have received a new contact request from ${name}
-
 * Details
     - Refrence Number: ${id}
     - Name: ${name}
@@ -97,7 +96,6 @@ You have received a new contact request from ${name}
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office"
     style="font-family:Nunito, Roboto, sans-serif">
-
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1" name="viewport">
@@ -115,12 +113,10 @@ You have received a new contact request from ${name}
         #outlook a {
             padding: 0;
         }
-
         .es-button {
             mso-style-priority: 100 !important;
             text-decoration: none !important;
         }
-
         a[x-apple-data-detectors] {
             color: inherit !important;
             text-decoration: none !important;
@@ -129,7 +125,6 @@ You have received a new contact request from ${name}
             font-weight: inherit !important;
             line-height: inherit !important;
         }
-
         .es-desk-hidden {
             display: none;
             float: left;
@@ -139,21 +134,17 @@ You have received a new contact request from ${name}
             line-height: 0;
             mso-hide: all;
         }
-
         [data-ogsb] .es-button {
             border-width: 0 !important;
             padding: 10px 40px 10px 40px !important;
         }
-
         @media only screen and (max-width:600px) {
-
             p,
             ul li,
             ol li,
             a {
                 line-height: 150% !important
             }
-
             h1,
             h2,
             h3,
@@ -162,107 +153,89 @@ You have received a new contact request from ${name}
             h3 a {
                 line-height: 120%
             }
-
             h1 {
                 font-size: 30px !important;
                 text-align: center
             }
-
             h2 {
                 font-size: 26px !important;
                 text-align: center
             }
-
             h3 {
                 font-size: 20px !important;
                 text-align: center
             }
-
             .es-header-body h1 a,
             .es-content-body h1 a,
             .es-footer-body h1 a {
                 font-size: 30px !important
             }
-
             .es-header-body h2 a,
             .es-content-body h2 a,
             .es-footer-body h2 a {
                 font-size: 26px !important
             }
-
             .es-header-body h3 a,
             .es-content-body h3 a,
             .es-footer-body h3 a {
                 font-size: 20px !important
             }
-
             .es-menu td a {
                 font-size: 16px !important
             }
-
             .es-header-body p,
             .es-header-body ul li,
             .es-header-body ol li,
             .es-header-body a {
                 font-size: 16px !important
             }
-
             .es-content-body p,
             .es-content-body ul li,
             .es-content-body ol li,
             .es-content-body a {
                 font-size: 16px !important
             }
-
             .es-footer-body p,
             .es-footer-body ul li,
             .es-footer-body ol li,
             .es-footer-body a {
                 font-size: 16px !important
             }
-
             .es-infoblock p,
             .es-infoblock ul li,
             .es-infoblock ol li,
             .es-infoblock a {
                 font-size: 12px !important
             }
-
             *[class="gmail-fix"] {
                 display: none !important
             }
-
             .es-m-txt-c,
             .es-m-txt-c h1,
             .es-m-txt-c h2,
             .es-m-txt-c h3 {
                 text-align: center !important
             }
-
             .es-m-txt-r,
             .es-m-txt-r h1,
             .es-m-txt-r h2,
             .es-m-txt-r h3 {
                 text-align: right !important
             }
-
             .es-m-txt-l,
             .es-m-txt-l h1,
             .es-m-txt-l h2,
             .es-m-txt-l h3 {
                 text-align: left !important
             }
-
             .es-m-txt-r img,
             .es-m-txt-c img,
             .es-m-txt-l img {
                 display: inline !important
             }
-
             .es-button-border {
                 display: block !important
             }
-
             a.es-button,
             button.es-button {
                 font-size: 20px !important;
@@ -270,13 +243,11 @@ You have received a new contact request from ${name}
                 border-left-width: 0px !important;
                 border-right-width: 0px !important
             }
-
             .es-adaptive table,
             .es-left,
             .es-right {
                 width: 100% !important
             }
-
             .es-content table,
             .es-header table,
             .es-footer table,
@@ -286,46 +257,36 @@ You have received a new contact request from ${name}
                 width: 100% !important;
                 max-width: 600px !important
             }
-
             .es-adapt-td {
                 display: block !important;
                 width: 100% !important
             }
-
             .adapt-img {
                 width: 100% !important;
                 height: auto !important
             }
-
             .es-m-p0 {
                 padding: 0px !important
             }
-
             .es-m-p0r {
                 padding-right: 0px !important
             }
-
             .es-m-p0l {
                 padding-left: 0px !important
             }
-
             .es-m-p0t {
                 padding-top: 0px !important
             }
-
             .es-m-p0b {
                 padding-bottom: 0 !important
             }
-
             .es-m-p20b {
                 padding-bottom: 20px !important
             }
-
             .es-mobile-hidden,
             .es-hidden {
                 display: none !important
             }
-
             tr.es-desk-hidden,
             td.es-desk-hidden,
             table.es-desk-hidden {
@@ -335,36 +296,28 @@ You have received a new contact request from ${name}
                 max-height: inherit !important;
                 line-height: inherit !important
             }
-
             tr.es-desk-hidden {
                 display: table-row !important
             }
-
             table.es-desk-hidden {
                 display: table !important
             }
-
             td.es-desk-menu-hidden {
                 display: table-cell !important
             }
-
             .es-menu td {
                 width: 1% !important
             }
-
             table.es-table-not-adapt,
             .esd-block-html table {
                 width: auto !important
             }
-
             table.es-social {
                 display: inline-block !important
             }
-
             table.es-social td {
                 display: inline-block !important
             }
-
             .es-desk-hidden {
                 display: table-row !important;
                 width: auto !important;
@@ -374,7 +327,6 @@ You have received a new contact request from ${name}
         }
     </style>
 </head>
-
 <body data-new-gr-c-s-loaded="14.1079.0"
     style="width:100%;font-family:Nunito, Roboto, sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0">
     <div class="es-wrapper-color" style="background-color:#222222">
@@ -918,14 +870,13 @@ You have received a new contact request from ${name}
 async function contact(req: NextApiRequest, res: NextApiResponse) {
     // Try the contact function
     try {
-        if (req?.method?.toString().toUpperCase() !== "POST") {
+        if (req?.method !== "POST") {
             return res.status(405).send("")
         }
 
         // Validations
         if (
             !req.body.name ||
-            !req.body.email ||
             !req.body.email ||
             !req.body.message ||
             process.env.HCAPTCHA_SECRET && !req.body.token
@@ -955,14 +906,15 @@ async function contact(req: NextApiRequest, res: NextApiResponse) {
 
 
         let response: {email?: boolean, telegram?: boolean} = {}
+        
+        if (process.env.EMAIL_ID) {
+            // Sending the message to Email ID
+            response = { ...response, email: await mail(id, name, email, ip, message) }
+        }
 
         if (process.env.BOT_TOKEN) {
             // Sending the message to the Telegram
             response = { ...response, telegram: await telegram(id, name, email, ip, message) }
-        }
-        if (process.env.EMAIL_ID) {
-            // Sending the message to Email ID
-            response = { ...response, email: await mail(id, name, email, ip, message) }
         }
 
         // Sending the response to the client
